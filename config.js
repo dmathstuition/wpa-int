0efef3635e01155d95e51e6afae35a00b2ccfe35
dmathstuition <dmathstuition@gmail.com>
2026-08-29 11:35:17 +0100

    Update APPS_SCRIPT_URL and TUTOR_NAME in config.js

--- config.js
+++ config.js
@@ -11,10 +11,10 @@ window.MATHQUEST_CONFIG = {
   // Paste your Apps Script Web App URL here (looks like:
   // https://script.google.com/macros/s/AKfy..../exec ).
   // Leave it blank to run in "offline practice" mode (nothing is sent).
-  APPS_SCRIPT_URL: "",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz_OczALSdd5wkzgKf_Ju5dCLq0pE5geLMpWiARGpSBEJhsmNmYj9kHk4fI7KeVfBV-gg/exec",
 
   // Tutor name that appears on the report (shown to "madam").
-  TUTOR_NAME: "Your Tutor",
+  TUTOR_NAME: "Mr Ola",
 
   // Turn sounds on/off site-wide (learners can also toggle in-game).
   SOUND_DEFAULT_ON: true
